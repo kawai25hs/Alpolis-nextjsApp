a7649ba70d7f00797e4eb727c003546bc3f3804b
Ka Wai <kawai25hs@gmail.com>
2024-05-25 19:08:12 +0800

    Updated Navbar & added font Jaro

--- app/components/navigation/navbar/index.tsx
+++ app/components/navigation/navbar/index.tsx
@@ -6,10 +6,35 @@ import Button from "./Button";
 const Navbar = ({ toggle }: { toggle: () => void }) => {
   return (
     <>
-      <div className="w-full h-16 bg-amber-50 sticky top-0">
-        <div className="container mx-auto px-4 h-full">
-          <div className="flex justify-between items-center h-full">
-            <Logo />
+      <div className="w-full h-16 bg-amber-50 sticky top-0 drop-shadow-md font-jaro text-lg flex justify-center">
+        <div className="container px-4 h-full">
+          <div className="flex justify-between md:justify-center items-center h-full">
+            <span className="md:hidden sm:mr-auto">
+              <Logo />
+            </span>
+
+            <ul className="hidden md:flex items-center gap-x-10 text-amber-600">
+              <li>
+                <Link href="/about">
+                  <p>About</p>
+                </Link>
+              </li>
+              <li>
+                <Link href="/introduction">
+                  <p>Introduction</p>
+                </Link>
+              </li>
+              <li>
+                <Logo />
+              </li>
+              <li>
+                Login
+              </li>
+              <li>
+                Create an account
+              </li>
+            </ul>
+
             <button
               type="button"
               className="inline-flex items-center md:hidden"
@@ -22,26 +47,12 @@ const Navbar = ({ toggle }: { toggle: () => void }) => {
                 viewBox="0 0 24 24"
               >
                 <path
-                  fill="#fff"
+                  fill="rgb(217 119 6)"
                   d="M3 6h18v2H3V6m0 5h18v2H3v-2m0 5h18v2H3v-2Z"
                 />
               </svg>
             </button>
-            <ul className="hidden md:flex gap-x-6 text-amber-600">
-              <li>
-                <Link href="/about">
-                  <p>About</p>
-                </Link>
-              </li>
-              <li>
-                <Link href="/introduction">
-                  <p>Introduction</p>
-                </Link>
-              </li>
-            </ul>
-            {/* <div className="hidden md:block">
-              <Button />
-            </div> */}
+
           </div>
         </div>
       </div>
